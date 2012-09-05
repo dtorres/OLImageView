863db6e6b5fd01438de151b05baf8f7cd4965250
Diego Torres <warorface@gmail.com>
2012-09-05 11:52:56 -0300

    Initial Commit

---
+++ OLImageViewDemo/OLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OLAppDelegate.h
+//  OLImageViewDemo
+//
+//  Created by Diego Torres on 9/5/12.
+//  Copyright (c) 2012 Onda Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OLImageViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OLImageViewDemo
+//
+//  Created by Diego Torres on 9/5/12.
+//  Copyright (c) 2012 Onda Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OLAppDelegate class]));
+    }
+}
